6911ca27aa73a5911128e8f0f3ef2cd80cfc60e3
Michel Reyes <mreyesh85@gmail.com>
2019-09-09 21:08:58 -0400

    Updates

--- precache-manifest.36f1fda73b8c5fb2596ac029bb4c2c49.js
+++ precache-manifest.36f1fda73b8c5fb2596ac029bb4c2c49.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c16020df045b98454726d1489187d1bb",
+    "revision": "ae1e83b65599d28b3d958e15fd59f286",
     "url": "/sbs/index.html"
   },
   {
-    "revision": "fb3e134c29f61eb46ae1",
+    "revision": "fc7a16873baa881000a1",
     "url": "/sbs/static/css/2.8c467cd4.chunk.css"
   },
   {
-    "revision": "57262452460bdc3510b5",
+    "revision": "8f94a4527be916e07984",
     "url": "/sbs/static/css/main.9098a0a8.chunk.css"
   },
   {
-    "revision": "fb3e134c29f61eb46ae1",
-    "url": "/sbs/static/js/2.40d316a1.chunk.js"
+    "revision": "fc7a16873baa881000a1",
+    "url": "/sbs/static/js/2.45b63c3d.chunk.js"
   },
   {
-    "revision": "57262452460bdc3510b5",
-    "url": "/sbs/static/js/main.5928bade.chunk.js"
+    "revision": "8f94a4527be916e07984",
+    "url": "/sbs/static/js/main.1acdf4ff.chunk.js"
   },
   {
     "revision": "10cb4f7453d9467f5dd7",
